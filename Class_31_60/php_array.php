a12b843bfb5e45577e35db3007dd4623ed17bd22
abdulla467a <ayanrahman009@gmail.com>
2022-03-07 23:26:59 +0600

    updated array

--- Class_31_60/php_array.php
+++ Class_31_60/php_array.php
@@ -1,6 +1,6 @@
 <?php
 
-  $colors = array("red","yellow","purple","green");
+ # $colors = array("red","yellow","purple","green");
 
   /*echo $colors[0]. "<br>";
 
@@ -10,9 +10,20 @@
 
   echo $colors[3]. "<br>";
 */
-echo "<pre>";
-print_r ($colors);
-echo "</pre>";
+#echo "<pre>";
+#print_r ($colors);
+#echo "</pre>";
+
+$colors = array("red","yellow","purple","green");
+
+echo "<ul>";
+for ($i = 0; $i <4; $i++){
+  echo "<li> $colors[$i] </li>";
+}
+
+echo "</ul>"
+
+
 
 
 
