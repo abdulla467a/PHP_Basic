71a1264d1068cb7c969b76b50ea964aefcdf9cd6
abdulla467a <ayanrahman009@gmail.com>
2022-03-08 16:08:22 +0600

    Count_array

---
+++ Class_31_60/count.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$food = array ('orange','pinapple','guava');
+
+echo "<pre>";
+print_r (array_count_values($food));
+
+echo "</pre>";
+
+#$len = count ($food);
+/*
+for ($i =0; $i < $len; $i++){
+    echo $food [$i] ."<br>";
+}
+*/
+?>
